e59da3caefb29aa3a717c3bfb98787519d422429
marinekev88 <km@kmdeveloping.com>
2021-10-09 12:09:52 -0700

    Updates

--- lib/index.ts
+++ lib/index.ts
@@ -1,36 +1,31 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export class Kms {
+  private readonly _url4: string;
+  private readonly _url6: string;
 
-    // Private Variables
-    private readonly _url4: string;
-    private readonly _url6: string;
+  constructor() {
+    this._url6 = 'https://api64.ipify.org';
+    this._url4 = 'https://api.ipify.org';
+  }
 
-    // Construct Endpoints
-    constructor() {
-        this._url6 = "https://api64.ipify.org";
-        this._url4 = "https://api.ipify.org";
-    }
+  private main = async (Url: string): Promise<AxiosResponse> => {
+    let config: AxiosRequestConfig;
+    config = {
+      method: 'GET',
+      url: Url,
+      headers: {
+        accept: 'application/json'
+      }
+    };
 
-    //private method
-    private main = async (Url: string): Promise<AxiosResponse> => {
-        let config: AxiosRequestConfig;
-        config = {
-            method: "GET",
-            url: Url,
-            headers: {
-                "accept": "application/json"
-            }
-        }
+    return await axios.request(config).then((res) => res.data);
+  };
 
-        return await axios.request(config).then(res => res.data);
-    }
-
-    //public methods
-    getIp4 = async (): Promise<AxiosResponse> => {
-        return await this.main(this._url4);
-    }
-    getIp6 = async (): Promise<AxiosResponse> => {
-        return await this.main(this._url6);
-    }
+  getIp4 = async (): Promise<AxiosResponse> => {
+    return await this.main(this._url4);
+  };
+  getIp6 = async (): Promise<AxiosResponse> => {
+    return await this.main(this._url6);
+  };
 }
