c24283eab69df99c6025072a87d6824099f4380b
marinekev88 <km@kmdeveloping.com>
2021-10-09 12:47:15 -0700

    update tests

--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
 const expect = require('chai').expect;
 const { Kms } = require('../dist/index');
-const index = new Kms()
+const _mockService = new Kms()
 
 describe('Return String Ip Address', () => {
     it('Should Be an ip Address string V4', async () => {
-        let res = await index.getIp4();
+        let res = await _mockService.getIp4();
 
         expect(res).to.be.a('string');
     });
 
     it('Should Be an ip Address string V6', async () => {
-        let res = await index.getIp6();
+        let res = await _mockService.getIp6();
 
         expect(res).to.be.a('string');
     });
