afe45b5becfb626c5f98259a25d87767b9d613f4
marinekev88 <km@kmdeveloping.com>
2021-10-20 19:31:31 -0700

    formatting

--- lib/Providers/Models/index.ts
+++ lib/Providers/Models/index.ts
@@ -1,30 +1,30 @@
 ﻿import { AxiosResponse } from 'axios';
 
 export type GodaddyBase = {
-    IpUpdate?: AxiosResponse | string;
-    RootDomain: string;
-    type: string;
-    url: string;
-    data: GodaddyDomain[];
+  IpUpdate?: AxiosResponse | string;
+  RootDomain: string;
+  type: string;
+  url: string;
+  data: GodaddyDomain[];
 };
 
 export type GodaddyDomain = {
-    name: string;
-    data?: AxiosResponse | string;
-    ttl: number;
-    type: string;
+  name: string;
+  data?: AxiosResponse | string;
+  ttl: number;
+  type: string;
 };
 
 export type DnsExitData = {
-    IpUpdate: AxiosResponse | string;
-    RootDomain: string;
-    DomainList: string[];
-    update?: DnsExitUpdate[];
+  IpUpdate: AxiosResponse | string;
+  RootDomain: string;
+  DomainList: string[];
+  update?: DnsExitUpdate[];
 };
 
 export type DnsExitUpdate = {
-    type: string;
-    name: string;
-    content: string | AxiosResponse;
-    ttl: number;
+  type: string;
+  name: string;
+  content: string | AxiosResponse;
+  ttl: number;
 };
