353a80ff1b23e658fa813a23e4cc246a298099fa
marinekev88 <km@kmdeveloping.com>
2021-10-20 19:28:48 -0700

    updates

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-import KmsIP from './Services/KmsIpHandler';
+import { KmsIP } from './Services/KmsIpHandler';
 import { GodaddyProvider, DnsExit } from './Providers';
 
 export { KmsIP, GodaddyProvider, DnsExit };
